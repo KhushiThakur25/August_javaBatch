e4bae487bfbaee718f891ef87b2e273bcc37e83e
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2024-08-16 13:50:43 +0530

    Add files via upload

---
+++ Input_class.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Input_class {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the string:");
+        String st1 = sc.next();
+        System.out.println("Enter the string 2");
+        String st2 = sc.nextLine();
+        System.out.println("Enter the number");
+        int x = sc.nextInt();
+        System.out.println("Enter the value");
+        float f = sc.nextFloat();
+
+    }
+}
